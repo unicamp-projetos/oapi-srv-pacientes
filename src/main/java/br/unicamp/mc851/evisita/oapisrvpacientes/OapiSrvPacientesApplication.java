38530e0d609531311617b2dab1b87e6a2ce9c4a8
Natan Rodrigues de Oliveira <natanrodriguesdeoliveira@gmail.com>
2021-06-05 10:18:20 -0300

    first commit

---
+++ src/main/java/br/unicamp/mc851/evisita/oapisrvpacientes/OapiSrvPacientesApplication.java
@@ -0,0 +1,13 @@
+package br.unicamp.mc851.evisita.oapisrvpacientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OapiSrvPacientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OapiSrvPacientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/unicamp/mc851/evisita/oapisrvpacientes/OapiSrvPacientesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unicamp.mc851.evisita.oapisrvpacientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OapiSrvPacientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
